ffb886d824e116854eb67bece6e720e2c1b6ac5a
Jeremiah Chinwe <jeremiahchinwe52@gmail.com>
2024-11-19 09:21:14 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <Button>Click Me</Button>
+      <h1 className='text-[80px] text-red-300'>My Name</h1>
+    </div>
+  )
+}
+
+export default App
