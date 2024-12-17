99850467f6aea62e8bb9e11108830122bd809791
Jeremiah Chinwe <jeremiahchinwe52@gmail.com>
2024-12-17 16:49:39 +0100

    Modified the Layout file

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 import { Toaster } from "./ui/toaster";
 import useTitle from "@/hooks/useTitle";
+import { useEffect } from "react";
+import useAuth from "@/hooks/useAuth";
 
 const Layout = () => {
   useTitle("Home Page")
+  const { userId } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!userId) {
+      navigate("/login")
+    }
+  }, [])
 
   return (
   <div className="layout">
