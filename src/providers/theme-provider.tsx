718c17b0901e9b78a9bc4db86acc99c34cd83be4
Jeremiah Chinwe <jeremiahchinwe52@gmail.com>
2024-12-20 01:44:32 +0100

    Modified the theme provider file

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -14,7 +14,7 @@ type ThemeProviderState = {
 }
 
 const initialState: ThemeProviderState = {
-  theme: "light",
+  theme: "system",
   setTheme: () => null,
 }
 
@@ -22,7 +22,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 
 export function ThemeProvider({
   children,
-  defaultTheme = "light",
+  defaultTheme = "system",
   storageKey = "notes-theme",
   ...props
 }: ThemeProviderProps) {
@@ -33,13 +33,13 @@ export function ThemeProvider({
   useEffect(() => {
     const root = window.document.documentElement
 
-    root.classList.remove("system", "dark")
+    root.classList.remove("light", "dark")
 
-    if (theme === "light") {
+    if (theme === "system") {
       const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
         .matches
         ? "dark"
-        : "system"
+        : "light"
 
       root.classList.add(systemTheme)
       return
