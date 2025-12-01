35790d022e4e75d51e7fff934eb7617b22f00a1a
Jeremiah Chinwe <chinwejeremiah9@gmail.com>
2025-12-01 04:53:46 -0800

    Adjusted layout

--- src/pages/ArchivedNotesPage.tsx
+++ src/pages/ArchivedNotesPage.tsx
@@ -11,13 +11,13 @@ const ArchivedNotesPage = () => {
   const [searchQuery, setSearchQuery] = useState("")
 
   return (
-    <section className="flex flex-col w-full bg-background rounded-t-md lg:rounded-t-none max-h-screen">
+    <section className="flex flex-col w-full bg-background rounded-t-md lg:rounded-t-none h-screen">
     <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
 
-    <article className="flex rounded-t-[1.25rem] lg:rounded-t-none bg-background flex-col lg:flex-row items-start justify-between lg:pr-[2rem] lg:pl-6 w-full">
+    <article className="flex rounded-t-[1.25rem] lg:rounded-t-none bg-background flex-col lg:flex-row items-start justify-between lg:pr-[2rem] lg:pl-3 w-full max-h-screen overflow-hidden">
      <ArchivedNotes />
 
-      <div className="basis-full w-full pb-5 lg:basis-[50%] lg:p-5">
+      <div className="basis-full max-h-screen w-full pb-5 lg:basis-[60%] lg:p-3 lg:pt-3.5">
         <Outlet />
       </div>
 
