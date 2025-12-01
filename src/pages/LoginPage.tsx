b62a46e10c9bc380ee6f55ebc32261825efb78c5
Jeremiah Chinwe <chinwejeremiah9@gmail.com>
2025-12-01 02:24:21 -0800

    Added self-hosted tinymce

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
     useTitle("Login")
     
     return (
-    <main className="bg-lightGray dark:bg-tagsBg flex justify-center items-center min-h-screen w-full px-4">
+    <main className="bg-lightGray dark:bg-tagsBg flex justify-center items-center min-h-screen w-full py-6 px-4">
         <LoginAuthForm />
     </main>
 )
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -5,7 +5,7 @@ const SignUpPage = () => {
   useTitle("Sign Up");
 
   return (
-    <main className="bg-lightGray dark:bg-tagsBg flex justify-center items-center min-h-screen w-full px-4">
+    <main className="bg-lightGray dark:bg-tagsBg flex justify-center items-center min-h-screen w-full py-6 px-4">
       <SignUpAuthForm />
     </main>
   );
